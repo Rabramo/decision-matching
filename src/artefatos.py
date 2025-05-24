626e59d6f52f5afc2278f97ec4d05602ffc9a45d
Rabramo <rabramo@gmail.com>
2025-05-23 22:58:37 -0300

    Correção de Artefato.py

--- src/artefatos.py
+++ src/artefatos.py
@@ -7,18 +7,16 @@ import scipy.sparse as sparse
 dados_processados = os.path.join('dados', 'processados')
 
 # Caminhos para cada artefato
-CAMINHO_VAGAS_PARQUET = os.path.join(dados_processados, 'vagas_cleaned.parquet')
+CAMINHO_VAGAS_META = os.path.join(dados_processados, 'vagas_cleaned.parquet')
 CAMINHO_CANDIDATOS_PARQUET = os.path.join(dados_processados, 'candidatos_cleaned.parquet')
 CAMINHO_VETORIZADOR = os.path.join(dados_processados, 'tfidf_vectorizer.joblib')
 CAMINHO_TFIDF_VAGAS = os.path.join(dados_processados, 'tfidf_vagas.npz')
 CAMINHO_TFIDF_CANDIDATOS = os.path.join(dados_processados, 'tfidf_candidatos.npz')
 
 
-def carregar_vagas_parquet() -> pd.DataFrame:
-    """
-    Carrega DataFrame de vagas limpas em Parquet.
-    """
-    return pd.read_parquet(CAMINHO_VAGAS_PARQUET)
+def carregar_vagas():
+    # se for JSON “linha a linha” (cada objeto numa linha):
+    return pd.read_json(CAMINHO_VAGAS_META, lines=True)
 
 
 def carregar_candidatos_parquet() -> pd.DataFrame:
